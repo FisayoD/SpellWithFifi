016ee29f6681f5c103486b2002701878e7d74e3f
FisayoD <ojofisayo24@gmail.com>
2021-10-12 14:34:58 +0100

    updates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn spellWithFifii.wsgi --log-file -
--- spellWithFifii/settings.py
+++ spellWithFifii/settings.py
@@ -125,3 +125,18 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
